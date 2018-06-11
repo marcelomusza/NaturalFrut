2d0b7a9ba7513c33f7e64273aec6d42494e570bd
Marcelo Muszalski <MarceloMusza@Gmail.com>
2018-06-11 01:09:52 -0300

    IngresarVenta View actualizado con más campos genericos, VentaViewModel creado y VentaController actualizado con más informacion

---
+++ NaturalFrut/App_BLL/ViewModels/VentaViewModel.cs
@@ -0,0 +1,33 @@
+﻿using NaturalFrut.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NaturalFrut.App_BLL.ViewModels
+{
+    public class VentaViewModel
+    {
+
+        public int? ID { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Prop1 { get; set; }
+
+        public string Prop2 { get; set; }
+
+        public string Prop3 { get; set; }
+        
+        public IEnumerable<Producto> Productos { get; set; }
+
+
+        public VentaViewModel()
+        {
+            ID = 0;
+        }
+
+
+
+    }
+}
--- NaturalFrut/Controllers/VentaController.cs
+++ NaturalFrut/Controllers/VentaController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NaturalFrut.App_BLL;
+using NaturalFrut.App_BLL.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,13 +10,40 @@ namespace NaturalFrut.Controllers
 {
     public class VentaController : Controller
     {
+
+        private readonly ClienteLogic clienteBL;
+        private readonly ProveedorLogic proveedorBL;
+        private readonly CommonLogic commonBL;
+        private readonly ProductoLogic productoBL;
+        private readonly VendedorLogic vendedorBL;
+
+        public VentaController(ClienteLogic ClienteLogic, CommonLogic CommonLogic, ProveedorLogic ProveedorLogic, ProductoLogic ProductoLogic, VendedorLogic VendedorLogic)
+        {
+            clienteBL = ClienteLogic;
+            commonBL = CommonLogic;
+            proveedorBL = ProveedorLogic;
+            productoBL = ProductoLogic;
+            vendedorBL = VendedorLogic;
+        }
+
         // GET: Venta
         public ActionResult Index()
         {
             return View();
         }
 
+        public ActionResult IngresarVenta()
+        {
 
+            var viewModel = new VentaViewModel();
+
+            var productos = productoBL.GetAllProducto();
+
+            viewModel.Productos = productos;
+
+
+            return View(viewModel);
+        }
 
 
     }
